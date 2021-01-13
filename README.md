50e5cbfc4f851b1fd5e8cb2f1e8c832472b4e13a
reachtostephen <histephen2015@gmail.com>
2021-01-14 00:11:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Iris-EDA-App
+A simple EDA Iris app with Streamlit
+
+#Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ iris_eda_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+	""" A Simple Iris EDA App """
+	st.title("Iris EDA App with Streamlit")
+	st.subheader("Streamlit is Cool")
+
+if __name__ == "__main__":
+	main()
